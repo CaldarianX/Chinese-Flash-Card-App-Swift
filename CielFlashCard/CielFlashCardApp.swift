8ae52be7a58abcf1771244f10ae8f6d7be6a2691
Tanakit <caldarianz@gmail.com>
2024-10-24 17:38:57 +0700

    Initial Commit

---
+++ CielFlashCard/CielFlashCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CielFlashCardApp.swift
+//  CielFlashCard
+//
+//  Created by Raphael on 24/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CielFlashCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CielFlashCardTests/CielFlashCardTests.swift
@@ -0,0 +1,17 @@
+//
+//  CielFlashCardTests.swift
+//  CielFlashCardTests
+//
+//  Created by Raphael on 24/10/24.
+//
+
+import Testing
+@testable import CielFlashCard
+
+struct CielFlashCardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
